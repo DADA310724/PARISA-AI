4150fae153c1a30b8b3e1478fa651e9f59b6d5ef
DADA310724 <dadavip2022@gmail.com>
2026-06-25 14:04:09 +0600

    fix: cache bump V11

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "PARISA-V10";
+const CACHE = "PARISA-V11";
 const STATIC = ["/", "/index.html", "/style.css", "/app.js", "/manifest.json", "/logo.jpg"];
 
 self.addEventListener("install", (e) => {
